dc7cf0b4b9d2aa6c9686af2e598695eb854c34a7
shuddy05 <horlabodehyibrahim@gmail.com>
2026-05-04 21:27:12 +0100

    latest

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import HeroSection from "../Components/HeroSection";
 import FeaturesProperties from "../Components/FeaturesProperties";
 import HomeComponents from "../Components/HomeComponents";
-import NavBar from "../Components/NavBar";
-import { useAuth } from "../context/AuthContext";
 const Home = () => {
-  const { user } = useAuth();
-  console.log("current user", user);
   return (
     <main>
       <HeroSection />
