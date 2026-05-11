9091a1a59d9ef0cd9d454fb62c4271eba2b45d92
shuddy05 <horlabodehyibrahim@gmail.com>
2026-05-11 21:21:38 +0100

    new commit

--- client/src/Components/PublicRoute.jsx
+++ client/src/Components/PublicRoute.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../context/AuthContext";
 
 const PublicRoute = ({ children }) => {
   const { user, loading } = useAuth();
-  if (loading) return <div>Loading...</div>;
+  // if (loading) return <div>Loading...</div>;
   if (user) return <Navigate to="/home" />;
   return children;
 };
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,19 +1,18 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
+  host: process.env.BREVO_HOST,
+  port: Number(process.env.BREVO_PORT),
   secure: false,
-  family: 4, 
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
   },
 });
 
 export const sendOTPEmail = async (email, otp) => {
   await transporter.sendMail({
-    from: `"Rentals" <${process.env.EMAIL_USER}>`,
+    from: `"Rentals" <${process.env.BREVO_USER}>`,
     to: email,
     subject: "Password Reset OTP",
     html: `
